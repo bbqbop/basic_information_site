8bc1cd2479f6c3b5257045f455e6f92e96d786a9
bbqbop <dierk.peters@gmail.com>
2024-03-26 15:29:46 -0400

    problem solved

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                res.end(content, 'utf8')
+            })
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf8')
+        }
+    })
+})
+
+
+const PORT = 6500;
+
+server.listen(PORT)
