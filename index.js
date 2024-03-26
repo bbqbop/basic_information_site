5e6e3d9a5ab356508a01bb62cc730a53c892f20b
bbqbop <dierk.peters@gmail.com>
2024-03-26 15:34:16 -0400

    status code correction

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const server = http.createServer((req, res) => {
     fs.readFile(filePath, (err, content) => {
         if (err) {
             fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
                 res.end(content, 'utf8')
             })
         } else {
